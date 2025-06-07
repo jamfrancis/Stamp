47d2af1f38c2909e1d435fa0bdb455864cfc1c27
jamfrancis <jamestelfordfrancis@gmail.com>
2025-06-07 15:52:50 -0500

    Initial Commit

---
+++ Stamp/StampApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampApp.swift
+//  Stamp
+//
+//  Created by James on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct StampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
